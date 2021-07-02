cda30e0ad95c80135d2f2070da433a081b21f835
Pauline <pauline.j.andersson@gmail.com>
2021-07-03 00:14:03 +0200

    change to heroku url

--- Frontend/src/pages/PublicProfile.js
+++ Frontend/src/pages/PublicProfile.js
@@ -38,7 +38,7 @@ const PublicProfile = () => {
   const { username } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:8080/user/${username}`)
+    fetch(`https://secure-escarpment-13722.herokuapp.com/user/${username}`)
       .then((res) => res.json())
       .then((json) => setUser(json));
   }, [username]);
--- Frontend/src/reducers/fetches/fetches.js
+++ Frontend/src/reducers/fetches/fetches.js
@@ -1,5 +1,4 @@
-//const BASE_URL = 'https://secure-escarpment-13722.herokuapp.com'
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://secure-escarpment-13722.herokuapp.com'
 
 const authHeaders =  {
     'content-type': 'application/json',
